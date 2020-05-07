d7877ad3a6d7d26838d380b090b53851591c661f
wallnutc <43997094+wallnutc@users.noreply.github.com>
2020-05-07 16:27:00 +0100

    push 2

--- oldsrc/student/components/fetchJSON.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from 'react';
-
-export default async function fetchJSON(url) {
-  useEffect(() => {
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data);
-    });
-}, []);
-}
